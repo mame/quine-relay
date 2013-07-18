ba85f3122bbfc2fd64d2401744bdf110b8366f66
Piotr <pioczarn@gmail.com>
2013-07-18 22:45:42 +0200

    Clean syntax and indentation

--- src/QR.rb.gen.rb
+++ src/QR.rb.gen.rb
@@ -5,25 +5,27 @@ CodeGen::List[0..-2].inject('%(eval$s=%q(#$s))') {|c, lang|
   lang.gen_code(c).tr(" \\","X`")
 }
 
+s = s.gsub("print","H").gsub("tring","J").gsub("main","K").gsub("``","^")
+
+# lookup of escapes and common names
+# 'H' % 9 => 0 (print)
+# 'J' % 9 => 2 (tring)
+# 'K' % 9 => 3 (main)
+# '^' % 9 => 4 (2 * backslash)
+# '`' % 9 => 6 (backslash)
+# 'X' % 9 => 7 (space)
 code = <<-END.split.join
   eval$s=%q(eval(%w(
 
-  #{CodeGen::PROLOGUE}
-  puts(eval(%q(#{
-    s.gsub("print","H").gsub("tring","J").gsub("main","K").gsub("``","^")
-    # 'H' % 9 => 0
-    # 'J' % 9 => 2
-    # 'K' % 9 => 3
-    # '^' % 9 => 4
-    # '`' % 9 => 6
-    # 'X' % 9 => 7
-  }).gsub(/[HJK^`X]/){[:print,0,:tring,:main,B*2,0,B,?\\s][$&.ord%9]}))
+    #{CodeGen::PROLOGUE}
+    puts(eval(
+      %q(#{ s }).gsub(/[HJK^`X]/){[:print,0,:tring,:main,B*2,0,B,?\\s][$&.ord%9]}
+    ))
 
-  
   )*""))
 END
 
-$stderr.puts "size: #{ code.size }"
+$stderr.puts "size: #{ code.b.size }"
 
 TEMPLATE = File.read("uroboros.txt")
 width = TEMPLATE[/.*/].size
