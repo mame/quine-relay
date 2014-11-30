28886dd6fa65a7674c58752aefd6f83ebedb9e96
Yusuke Endoh <mame@ruby-lang.org>
2014-11-30 19:22:23 +0900

    * try "apt-get update" before install.

--- src/dot.travis.yml.gen.rb
+++ src/dot.travis.yml.gen.rb
@@ -15,6 +15,8 @@ yaml["language"] = "ruby"
 yaml["rvm"] = ["2.1.0"]
 yaml["env"] = "PATH=/usr/games:$PATH"
 yaml["before_install"] = [
+  "sudo apt-get update -qq",
+  'sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade -qq',
   "sudo apt-get install -qq #{ apts.join(" ") }",
 ]
 yaml["before_script"] = [
