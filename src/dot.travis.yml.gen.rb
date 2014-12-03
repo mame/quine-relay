ec0429b00cf173311360e706cdb55354938ad5a8
Yusuke Endoh <mame@ruby-lang.org>
2014-12-04 00:43:52 +0900

    * add npiet build dependencies to .travis.yml.
    Looks refactoring is needed...

--- src/dot.travis.yml.gen.rb
+++ src/dot.travis.yml.gen.rb
@@ -2,6 +2,8 @@ require "yaml"
 require_relative "code-gen"
 
 apts = CodeGen::List.reverse.flat_map {|c| c.steps.map {|step| step.apt } }
+other_packages = %w(cmake libpng12-dev libgd2-xpm-dev groff)
+
 
 utopic_to_precise = {
   "clojure1.4" => "clojure",
@@ -17,7 +19,7 @@ yaml["env"] = "PATH=/usr/games:$PATH"
 yaml["before_install"] = [
   "sudo apt-get update -qq",
   'sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade -qq',
-  "sudo apt-get install -qq #{ apts.join(" ") }",
+  "sudo apt-get install -qq #{ [*apts.flatten.compact.uniq, *other_packages].sort * " " }",
   "sudo service postgresql stop",
   "sudo service mysql stop",
 ]
