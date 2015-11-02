dc6ad4236c10789afeb236bd7d4f6929f7bdc02a
Yusuke Endoh <mame@ruby-lang.org>
2015-11-03 02:33:40 +0900

    * improve the performance of vendor/blc.rb
    Use call-by-need instead of call-by-name.
    Array is faster than Struct...

--- vendor/blc.rb
+++ vendor/blc.rb
@@ -1,23 +1,18 @@
 # Binary lambda calculus interpreter
 #
-# (C) Copyright 2014, Yusuke Endoh
+# (C) Copyright 2014, 2015, Yusuke Endoh
 # License: MIT
 #
-# See in detail: http://homepages.cwi.nl/~tromp/cl/cl.html
+# See in detail: http://tromp.github.io/cl/cl.html
 
 # BLC8 or not
 binary_mode = ARGV.empty?
 $stdout.sync = true
 
 # abstract syntax tree
-Var = Struct.new(:num)
-Abs = Struct.new(:exp) # Abs[nil] represents input list
-App = Struct.new(:exp1, :exp2)
-Out = Struct.new(:type)
-
-cons = ->(a, b) { Abs[App[App[Var[0], a], b]] }
-bool = ->(b) { Abs[Abs[Var[b ? 1 : 0]]] }
-read = ->(i) { i > 0 ? cons[bool[get_bit[]], read[i - 1]] : bool[false] }
+Var = -> v { [:var, v] }
+Abs = -> e { [:abs, e] }
+App = -> e1, e2 { [:app, e1, e2] }
 
 # bit stream
 current_byte, bit_offset = nil, 0
@@ -31,14 +26,8 @@ get_bit = -> do
   current_byte ? current_byte[bit_offset] == 0 : false
 end
 
-# deconstructor of list of boolean lists (for BLC8 I/O)
-E1 = Abs[App[Var[0], cons[cons[Out[0], Out[1]], Out[nil]]]]
-# deconstructor of boolean list
-E2 = Abs[App[Var[0], cons[Out[0], Out[1]]]]
-
 # parse
-stack = [:exp]
-acc = nil
+stack, acc = [:exp], nil
 until stack.empty?
   v = stack.pop
   acc = case v
@@ -56,43 +45,55 @@ until stack.empty?
   else App[v, acc] # parsing two exps of app finished
   end
 end
-term = App[binary_mode ? E1 : E2, App[acc, Abs[nil]]]
 bit_offset = 0 # force to align bit offset
 
-# evaluate
-env, out, stack = nil, 0, []
-Closure = Struct.new(:term, :env, :next)
+# I/O
+cons = -> a, b { Abs[App[App[Var[0], a], b]] }
+bool = -> b { Abs[Abs[Var[b ? 1 : 0]]] }
+if binary_mode
+  read = -> i { i > 0 ? cons[bool[get_bit[]], read[i - 1]] : bool[false] }
+  in_ = [:in, -> { get_byte[] ? cons[read[8], in_] : bool[false] }]
+  outc = 0
+  out0, out1 = [0, 1].map {|n| [:out, -> { outc = (2 * outc + n) & 255 }] }
+  out0[2] = out1[2] = App[Var[0], cons[out0, out1]]
+  outn = [:out, -> { putc(outc) }]
+  out_ = outn[2] = App[Var[0], cons[cons[out0, out1], outn]]
+else
+  in_ = [:in, -> { get_byte[] ? cons[bool[get_bit[]], in_] : bool[false] }]
+  out0, out1 = [0, 1].map {|n| [:out, -> { print(n) }] }
+  out_ = out0[2] = out1[2] = App[Var[0], cons[out0, out1]]
+end
+
+# eval-loop
+exp = App[Abs[out_], App[acc, in_]]
+env, stack = [], [[nil, nil]]
 while true
-  case term
-  when Var
-    term.num.times { env = env.next }
-    term, env = env.term, env.env
-  when App
-    stack << Closure[term.exp2, env]
-    term = term.exp1
-  when Abs
-    if term.exp
-      exit if stack.empty?
-      stack.last.next = env
-      term, env = term.exp, stack.pop
-    else
-      # input
-      term.exp = (get_byte[] ?
-                  cons[binary_mode ? read[8] : bool[get_bit[]], Abs[nil]] :
-                  bool[false]).exp
+  case exp[0]
+  when :var
+    exp[1].times { env = env[2] }
+    if env.frozen?
+      valexp, valenv = env # already forced
+    else # not forced yet
+      stack.last << env
+      exp, env = env[0], env[1]
+      next
     end
-  when Out
-    if binary_mode
-      if term.type
-        out = (2 * out | term.type) & 255
-        term = E2
-      else
-        putc(out)
-        term = E1
-      end
-    else
-      print(term.type)
-      term = E2
-    end
+  when :app
+    stack << [exp[2], env]
+    next exp = exp[1]
+  when :abs
+    valexp, valenv = exp[1], env
+  when :in  then next exp = exp[1][]
+  when :out then exp[1][]; valexp, valenv = exp[2], env
   end
+  env = stack.pop
+  until env.size == 2
+    th = env.pop
+    th[0] = valexp # force
+    th[1] = valenv
+    th.freeze
+  end
+  break unless env[0]
+  exp = valexp
+  env[2] = valenv
 end
