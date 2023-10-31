26066d1ede0b91720baceecbd99dbdf7e4d3240b
Yusuke Endoh <mame@ruby-lang.org>
2023-11-01 05:39:34 +0900

    Update copyright year

--- src/README.md.gen.rb
+++ src/README.md.gen.rb
@@ -341,7 +341,7 @@ See `vendor/README` in detail.
 
 The MIT License (MIT)
 
-Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022 Yusuke Endoh (@mametter), @hirekoke
+Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023 Yusuke Endoh (@mametter), @hirekoke
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
